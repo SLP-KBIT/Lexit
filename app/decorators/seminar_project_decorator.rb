4fba637e3890c3d22b0d1d83b856f8bb22351856
Makoto NAKAYA <nakaya@aqte.net>
2015-03-03 16:12:35 +0900

    輪行プロジェクト一覧 書籍名を表示するようにした。

--- app/decorators/seminar_project_decorator.rb
+++ app/decorators/seminar_project_decorator.rb
@@ -5,4 +5,17 @@ class SeminarProjectDecorator < Draper::Decorator
     object.description.gsub(/</, '&lt;').gsub(/>/, '&gt;').gsub(/&/, '&amp;').gsub(/\n/, '<br/>')
   end
 
+  def book_name
+    if object.books.count > 0
+      name = object.books.first.name
+      if name.blank?
+        '書籍名が未設定'
+      else
+        name
+      end
+    else
+      '書籍が未設定'
+    end
+  end
+
 end
