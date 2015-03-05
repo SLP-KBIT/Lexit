0e17633083afcefeceb58a88fa5d13e452656d0b
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2015-03-06 03:56:57 +0900

    entryの削除を実装 プロジェクト詳細ページで、参加・検討の有無によって表示を変更

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,20 @@
 class EntriesController < ApplicationController
+  before_action :load_entry
   def create
+    @entry.destroy unless @entry.nil?
     entry = Entry.new(seminar_project_id: params[:seminar_project_id], user_id: current_user.id, entry_type: params[:entry_type])
     entry.save!
     redirect_to seminar_project_path(params[:seminar_project_id])
+  end
+
+  def destroy
+    @entry.destroy
+    redirect_to seminar_project_path(params[:seminar_project_id])
+  end
+
+  private
+
+  def load_entry
+    @entry = Entry.where(seminar_project_id: params[:seminar_project_id], user_id: current_user.id).first
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,14 @@ class User < ActiveRecord::Base
   has_many :owned_seminar_projects, class_name: 'SeminarProject'
   has_many :entries
   has_many :joined_seminar_projects, through: :entries, source: :seminar_project
+
+  def participant?(seminar_project)
+    return false if entries.is_participant.where(seminar_project_id: seminar_project.id).blank?
+    true
+  end
+
+  def thinking?(seminar_project)
+    return false if entries.is_thinking.where(seminar_project_id: seminar_project.id).blank?
+    true
+  end
 end
