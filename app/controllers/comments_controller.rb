544d710bd740116809d634a14396caef91fe8de1
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2015-09-30 15:32:33 +0900

    コメントの生成方法のリファクタリング

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.new(comment_params)
-    comment.user_id = current_user.id
+    comment = current_user.comments.build(comment_params)
     comment.save!
     redirect_to seminar_project_path(id: comment.target_id)
   end
